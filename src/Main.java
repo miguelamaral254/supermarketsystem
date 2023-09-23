7f51735815355121e2f4bc59b7da74c052ea269c
Miguel Amaral <118134009+miguelamaral254@users.noreply.github.com>
2023-09-22 21:50:00 -0300

    Update Main.java
    update desconto

--- src/Main.java
+++ src/Main.java
@@ -10,23 +10,23 @@ public class Main {
 
         while (true) {
             System.out.println("----------------------------------------------");
-            System.out.println("1 - adicionar um item");
-            System.out.println("2 - remover um item");
-            System.out.println("3 - alterar um item");
-            System.out.println("4 - visualizar seu carrinho de compras");
-            System.out.println("5 - visualizar total a pagar");
-            System.out.println("6 - aplicar desconto");
-            System.out.println("7 - sair");
+            System.out.println("1 - Adicionar um item");
+            System.out.println("2 - Remover um item");
+            System.out.println("3 - Alterar um item");
+            System.out.println("4 - Visualizar seu carrinho de compras");
+            System.out.println("5 - Visualizar total a pagar");
+            System.out.println("6 - Aplicar desconto");
+            System.out.println("7 - Sair");
             System.out.println("----------------------------------------------");
             try {
                 int escolha = scanner.nextInt();
                 switch (escolha) {
                     case 1:
                         try {
-                            System.out.print("nome do item: ");
+                            System.out.print("Nome do item: ");
                             scanner.nextLine(); // Consume newline
                             String nome = scanner.nextLine();
-                            System.out.print("preço do item: ");
+                            System.out.print("Preço do item: ");
                             float preco = scanner.nextFloat();
                             Item item = new Item(nome, preco);
                             carrinhoDeCompras.put(nome, item);
@@ -37,7 +37,7 @@ public class Main {
                         break;
                     case 2:
                         try {
-                            System.out.print("nome do item que deseja remover: ");
+                            System.out.print("Nome do item que deseja remover: ");
                             scanner.nextLine(); // Consume newline
                             String nomeToRemove = scanner.nextLine();
                             carrinhoDeCompras.remove(nomeToRemove);
@@ -48,24 +48,24 @@ public class Main {
                         break;
                     case 3:
                         try {
-                            System.out.print("qual item você deseja alterar (pelo nome)? ");
+                            System.out.print("Qual item você deseja alterar (pelo nome)? ");
                             scanner.nextLine(); // Consume newline
                             String nomeToAlter = scanner.nextLine();
                             if (carrinhoDeCompras.containsKey(nomeToAlter)) {
-                                System.out.println("item " + nomeToAlter + " selecionado!");
-                                System.out.print("você deseja mudar o nome ou o preço? ");
+                                System.out.println("Item " + nomeToAlter + " selecionado!");
+                                System.out.print("Você deseja mudar o nome ou o preço? ");
                                 String escolhaAlteracao = scanner.nextLine();
                                 if (escolhaAlteracao.equals("nome")) {
-                                    System.out.print("novo nome: ");
+                                    System.out.print("Novo nome: ");
                                     String novoNome = scanner.nextLine();
                                     carrinhoDeCompras.get(nomeToAlter).setNome(novoNome);
-                                    System.out.println("o nome do item foi alterado para " + novoNome + " com sucesso!");
+                                    System.out.println("O nome do item foi alterado para " + novoNome + " com sucesso!");
                                 } else if (escolhaAlteracao.equals("preco")) {
                                     try {
-                                        System.out.print("novo preço: ");
+                                        System.out.print("Novo preço: ");
                                         float novoPreco = scanner.nextFloat();
                                         carrinhoDeCompras.get(nomeToAlter).setPreco(novoPreco);
-                                        System.out.println("o preço do item foi alterado para " + novoPreco + " com sucesso!");
+                                        System.out.println("O preço do item foi alterado para " + novoPreco + " com sucesso!");
                                     } catch (Exception e) {
                                         System.out.println("Não foi possível alterar o preço do item");
                                     }
@@ -80,30 +80,40 @@ public class Main {
                         }
                         break;
                     case 4:
-                        System.out.println("Carrinho de Compras:");
-                        for (Map.Entry<String, Item> entry : carrinhoDeCompras.entrySet()) {
-                            String nomeItem = entry.getKey();
-                            Item item = entry.getValue();
-                            System.out.println("Nome: " + nomeItem + " | Preço: " + item.getPreco());
+                        if (carrinhoDeCompras.isEmpty()) {
+                            System.out.println("Não há itens no carrinho.");
+                        } else {
+                            System.out.println("Carrinho de Compras:");
+                            for (Map.Entry<String, Item> entry : carrinhoDeCompras.entrySet()) {
+                                String nomeItem = entry.getKey();
+                                Item item = entry.getValue();
+                                System.out.println("Nome: " + nomeItem + " | Preço: " + item.getPreco());
+                            }
                         }
                         break;
                     case 5:
-                        float totalAPagar = calcularTotalAPagar(carrinhoDeCompras);
-                        System.out.println("Total a Pagar: " + totalAPagar);
+                        if (carrinhoDeCompras.isEmpty()) {
+                            System.out.println("Não há itens no carrinho.");
+                        } else {
+                            float totalAPagar = calcularTotalAPagar(carrinhoDeCompras);
+                            System.out.println("Total a Pagar: " + totalAPagar);
+                        }
                         break;
                     case 6:
                         try {
                             System.out.print("Digite o valor do desconto: ");
                             float desconto = scanner.nextFloat();
-                            float totalAntesDoDesconto = calcularTotalAPagar(carrinhoDeCompras);
-                            float totalComDesconto = totalAntesDoDesconto - desconto;
-                            if (totalComDesconto < 0) {
-                                System.out.println("O desconto resultaria em um total negativo. O desconto não foi aplicado.");
+                            if (desconto >= 0 && desconto <= 100) {
+                                float totalAPagar = calcularTotalAPagar(carrinhoDeCompras);
+                                float valorDesconto = (desconto / 100) * totalAPagar;
+                                float totalComDesconto = totalAPagar - valorDesconto;
+                                System.out.println("Desconto aplicado: " + desconto + "%");
+                                System.out.println("Total a Pagar com Desconto: " + totalComDesconto);
                             } else {
-                                System.out.println("Total com Desconto: " + totalComDesconto);
+                                System.out.println("O desconto deve ser um valor entre 0 e 100.");
                             }
                         } catch (Exception e) {
-                            System.out.println("Valor de desconto inválido.");
+                            System.out.println("Digite um valor válido");
                         }
                         break;
                     case 7:
