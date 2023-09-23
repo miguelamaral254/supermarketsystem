24a13d782be419a8551b0bb0a99ed6f84faf4fef
Miguel Amaral <118134009+miguelamaral254@users.noreply.github.com>
2023-09-22 22:30:06 -0300

    Update Main.java
    Add- retornos "Não há itens no carrinho" caso as case 2, 3 ,4, 5 e 6 estejam vazias.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
-import java.util.HashMap;
-import java.util.Map;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Map<String, Item> carrinhoDeCompras = new HashMap<>();
+        List<Item> carrinhoDeCompras = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        int proximoNumero = 1; // Variável para controlar o próximo número a ser exibido
 
         while (true) {
             System.out.println("----------------------------------------------");
@@ -26,47 +28,67 @@ public class Main {
                         scanner.nextLine();
                         String nome = scanner.nextLine();
 
-                        if (carrinhoDeCompras.containsKey(nome)) {
-                            System.out.println("Item já adicionado na lista, escolha outro por gentileza.");
+                        if (itemExiste(carrinhoDeCompras, nome)) {
+                            System.out.println("Item já adicionado na lista.");
                         } else {
-                            System.out.print("Preço do item(em R$): ");
+                            System.out.print("Preço do item: ");
                             float preco = scanner.nextFloat();
                             Item item = new Item(nome, preco);
-                            carrinhoDeCompras.put(nome, item);
+                            carrinhoDeCompras.add(item);
                             System.out.println(nome + " foi adicionado ao carrinho de compras!");
                         }
                         break;
                     case 2:
-                        System.out.print("Nome do item que deseja remover: ");
-                        scanner.nextLine();
-                        String nomeToRemove = scanner.nextLine();
-                        carrinhoDeCompras.remove(nomeToRemove);
-                        System.out.println("Item removido com sucesso!");
+                        if (carrinhoDeCompras.isEmpty()) {
+                            System.out.println("Não há itens no carrinho.");
+                        } else {
+                            // Mostrar a lista de itens
+                            System.out.println("Itens no carrinho:");
+                            for (int i = 0; i < carrinhoDeCompras.size(); i++) {
+                                Item currentItem = carrinhoDeCompras.get(i);
+                                System.out.println(proximoNumero + ": " + currentItem.getNome() + " - Preço: " + currentItem.getPreco());
+                                proximoNumero++;
+                            }
+
+                            System.out.print("Qual item você deseja remover (pela ordem na lista)? ");
+                            int itemToRemove = scanner.nextInt();
+                            if (itemToRemove >= 1 && itemToRemove <= carrinhoDeCompras.size()) {
+                                carrinhoDeCompras.remove(itemToRemove - 1);
+                                System.out.println("Item removido com sucesso!");
+                            } else {
+                                System.out.println("Número de item inválido.");
+                            }
+                        }
                         break;
                     case 3:
                         if (carrinhoDeCompras.isEmpty()) {
                             System.out.println("Não há itens no carrinho.");
                         } else {
-                            System.out.print("Qual item você deseja alterar (pelo nome)? ");
-                            scanner.nextLine();
-                            String nomeToAlter = scanner.nextLine();
-                            if (carrinhoDeCompras.containsKey(nomeToAlter)) {
-                                System.out.println("Item " + nomeToAlter + " selecionado!");
+                            // Mostrar a lista de itens
+                            System.out.println("Itens no carrinho:");
+                            for (int i = 0; i < carrinhoDeCompras.size(); i++) {
+                                Item currentItem = carrinhoDeCompras.get(i);
+                                System.out.println(proximoNumero + ": " + currentItem.getNome() + " - Preço: " + currentItem.getPreco());
+                                proximoNumero++;
+                            }
+
+                            System.out.print("Qual item você deseja alterar (pela ordem na lista)? ");
+                            int itemToAlter = scanner.nextInt();
+                            if (itemToAlter >= 1 && itemToAlter <= carrinhoDeCompras.size()) {
+                                Item itemExistente = carrinhoDeCompras.get(itemToAlter - 1);
+                                System.out.println("Item " + itemExistente.getNome() + " selecionado!");
                                 System.out.print("Você deseja mudar o nome ou o preço? ");
+                                scanner.nextLine();
                                 String escolhaAlteracao = scanner.nextLine();
                                 if (escolhaAlteracao.equals("nome")) {
                                     System.out.print("Novo nome: ");
                                     String novoNome = scanner.nextLine();
-                                    Item itemExistente = carrinhoDeCompras.get(nomeToAlter);
                                     itemExistente.setNome(novoNome);
-                                    carrinhoDeCompras.remove(nomeToAlter); // Remova o item antigo
-                                    carrinhoDeCompras.put(novoNome, itemExistente); // Adicione o item com o novo nome
                                     System.out.println("O nome do item foi alterado para " + novoNome + " com sucesso!");
-                                } else if (escolhaAlteracao.equals("preco(R$)")) {
+                                } else if (escolhaAlteracao.equals("preco")) {
                                     try {
                                         System.out.print("Novo preço: ");
                                         float novoPreco = scanner.nextFloat();
-                                        Item itemExistente = carrinhoDeCompras.get(nomeToAlter);
                                         itemExistente.setPreco(novoPreco);
                                         System.out.println("O preço do item foi alterado para " + novoPreco + " com sucesso!");
                                     } catch (Exception e) {
@@ -76,41 +98,46 @@ public class Main {
                                     System.out.println("Opção inválida. Nenhum dado foi alterado.");
                                 }
                             } else {
-                                System.out.println("Este item não existe");
+                                System.out.println("Número de item inválido.");
                             }
                         }
                         break;
                     case 4:
                         if (carrinhoDeCompras.isEmpty()) {
-                            System.out.println("Não há itens no carrinho.");
+                            System.out.println("Ainda não há itens no carrinho.");
                         } else {
+                            // Mostrar a lista de itens
                             System.out.println("Carrinho de Compras:");
-                            for (Map.Entry<String, Item> entry : carrinhoDeCompras.entrySet()) {
-                                String nomeItem = entry.getKey();
-                                Item itemExistente = entry.getValue();
-                                System.out.println("Nome: " + nomeItem + " | Preço: " + itemExistente.getPreco());
+                            for (int i = 0; i < carrinhoDeCompras.size(); i++) {
+                                Item currentItem = carrinhoDeCompras.get(i);
+                                System.out.println(proximoNumero + ": " + currentItem.getNome() + " - Preço: " + currentItem.getPreco());
+                                proximoNumero++;
                             }
                         }
                         break;
                     case 5:
                         if (carrinhoDeCompras.isEmpty()) {
-                            System.out.println("Não há itens no carrinho.");
+                            System.out.println("Ainda não há itens no carrinho.");
                         } else {
                             float totalAPagar = calcularTotalAPagar(carrinhoDeCompras);
                             System.out.println("Total a Pagar: " + totalAPagar);
                         }
                         break;
                     case 6:
-                        System.out.print("Digite o valor do desconto: ");
-                        float desconto = scanner.nextFloat();
-                        if (desconto >= 0 && desconto <= 100) {
-                            float totalAPagar = calcularTotalAPagar(carrinhoDeCompras);
-                            float valorDesconto = (desconto / 100) * totalAPagar;
-                            float totalComDesconto = totalAPagar - valorDesconto;
-                            System.out.println("Desconto aplicado: " + desconto + "%");
-                            System.out.println("Total a Pagar com Desconto: " + totalComDesconto);
+                        if (carrinhoDeCompras.isEmpty()) {
+                            System.out.println("Ainda não há itens no carrinho.");
                         } else {
-                            System.out.println("O desconto deve ser um valor entre 0 e 100.");
+                            System.out.print("Digite o valor do desconto: ");
+                            float desconto = scanner.nextFloat();
+                            if (desconto >= 0 && desconto <= 100) {
+                                float totalAPagar = calcularTotalAPagar(carrinhoDeCompras);
+                                float valorDesconto = (desconto / 100) * totalAPagar;
+                                float totalComDesconto = totalAPagar - valorDesconto;
+                                System.out.println("Desconto aplicado: " + desconto + "%");
+                                System.out.println("Total a Pagar com Desconto: " + totalComDesconto);
+                            } else {
+                                System.out.println("O desconto deve ser um valor entre 0 e 100.");
+                            }
                         }
                         break;
                     case 7:
@@ -126,9 +153,18 @@ public class Main {
         }
     }
 
-    private static float calcularTotalAPagar(Map<String, Item> carrinhoDeCompras) {
+    private static boolean itemExiste(List<Item> carrinhoDeCompras, String nome) {
+        for (Item item : carrinhoDeCompras) {
+            if (item.getNome().equals(nome)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static float calcularTotalAPagar(List<Item> carrinhoDeCompras) {
         float total = 0;
-        for (Item item : carrinhoDeCompras.values()) {
+        for (Item item : carrinhoDeCompras) {
             total += item.getPreco();
         }
         return total;
@@ -160,3 +196,4 @@ class Item {
         this.preco = preco;
     }
 }
+
